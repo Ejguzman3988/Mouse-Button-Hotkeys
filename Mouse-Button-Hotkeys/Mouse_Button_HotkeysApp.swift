6692df3071ec9a06660dd2ed930f5167db8dd07e
ejguzman3988 <eriberto.guzman@outlook.com>
2021-04-28 22:45:38 -0400

    Initial Commit

---
+++ Mouse-Button-Hotkeys/Mouse_Button_HotkeysApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mouse_Button_HotkeysApp.swift
+//  Mouse-Button-Hotkeys
+//
+//  Created by Eriberto Guzman on 4/28/21.
+//
+
+import SwiftUI
+
+@main
+struct Mouse_Button_HotkeysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
